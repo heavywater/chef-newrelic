607dcbf5e249d1ef90a1e7e3e7f18be997137ffa
Cameron Johnston <cameron@needle.com>
2012-05-21 11:42:54 -0600

    adding :failure_ok attribute to newrelic_deploy resource, defaults to true (cherry picked from commit 4bd34b6e70a484d0da74e65a851b9a9a3a07399f)

--- libraries/deploy.rb
+++ libraries/deploy.rb
@@ -19,8 +19,12 @@ module Opscode
         response = self.class.post('/deployments.xml', @options)
 
         if response.has_key?('errors')
-          Chef::Log.error("New Relic deploy notification error: #{response['errors']['error']}")
-          raise
+          if @new_resource.failure_ok
+            Chef::Log.error("New Relic deploy notification error: #{response['errors']['error']}")
+          else
+            Chef::Log.error("New Relic deploy notification error: #{response['errors']['error']}")
+            raise
+          end
         end
 
       end
--- resources/deploy.rb
+++ resources/deploy.rb
@@ -8,3 +8,4 @@ actions :notify
 attribute :key, :kind_of => String, :required => true
 attribute :app, :kind_of => String, :required => false
 attribute :name, :kind_of => String, :required => true, :name_attribute => true
+attribute :failure_ok, :kind_of => [ TrueClass, FalseClass ], :default => true
